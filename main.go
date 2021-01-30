f8814bfd6b0d4aa9e17e303ebfbddfebc99c4242
nmerkulov <nikita_merkulov@epam.com>
2021-01-30 22:38:54 +0300

    some file renamig;delete extra prints

--- main.go
+++ main.go
@@ -33,3 +33,14 @@ func main() {
 	}
 	wg.Wait()
 }
+
+func generateInts(count int, f func() int) <-chan int {
+	ch := make(chan int)
+	go func() {
+		defer close(ch)
+		for i := 0; i < count; i++ {
+			ch <- f()
+		}
+	}()
+	return ch
+}
